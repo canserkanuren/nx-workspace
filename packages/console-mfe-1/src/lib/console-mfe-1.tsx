4e80dba65fb84d10a7364d8190ce75eb8ea2da68
canserkanuren <canserkan.uren@gmail.com>
2022-04-21 21:28:22 +0200

    :bug:

--- packages/console-mfe-1/src/lib/console-mfe-1.tsx
+++ packages/console-mfe-1/src/lib/console-mfe-1.tsx
@@ -7,7 +7,7 @@ export interface ConsoleMfe1Props {}
 export function ConsoleMfe1(props: ConsoleMfe1Props) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to ConsoleMfe1!</h1>
+      <h1>Welcome to ConsoleMfe1! My beautiful mfe!!</h1>
       <Alert>Hello this is an alert</Alert>
     </div>
   );
